5216bbb4f8b44fb2870503359310e94acafa5359
ColonelPan1k <kyle.goldbeck@gmail.com>
2023-02-28 15:05:23 -0500

    feat: update main.py

--- prototype/src/main.py
+++ prototype/src/main.py
@@ -1,12 +1,54 @@
-import sqlalchemy as sa
+import fastapi
+from fastapi.middleware.cors import CORSMiddleware
 
-engine = sa.create_engine("mysql+pymysql://user:password@172.19.0.1:3306/db")
+import data_access
+import upcoming
 
-with engine.begin() as conn:
-    qry = """
-    SELECT * FROM circuits
-    """
+app = fastapi.FastAPI()
 
-    res = conn.execute(sa.text(qry)).all()
+origins = ["*"]
 
-    print(res)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/upcoming")
+async def upcoming_event():
+    return {
+        "event": upcoming.process_upcoming_event_data(),
+        "next_event": upcoming.calculate_upcoming_sub_event(),
+    }
+
+
+@app.get("/standings/{year}/{race_name}")
+async def get_standings(year: int, race_name: str):
+    return data_access.get_season_standings(year, race_name)
+
+
+@app.get("/season")
+async def season():
+    return data_access.get_season_schedule()
+
+
+@app.get("/season-standings/{year}/{race_name}")
+async def season_standings(year: int, race_name: str):
+    return data_access.get_season_standings(year, race_name)
+
+
+@app.get("/table/{year}")
+async def season_table(year: int):
+    return data_access.get_season_table(year)
+
+
+@app.get("/race/{race_id}")
+async def get_race_info(race_id: int):
+    ...
+
+
+if __name__ == "__main__":
+    app()
