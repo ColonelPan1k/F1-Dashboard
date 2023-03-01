6aff1009b7dcdf27d8a905e72647536a161500ae
ColonelPan1k <kyle.goldbeck@gmail.com>
2023-03-01 15:56:33 -0500

    chore: dump current `app.js` state

--- f1-dashboard-frontend/src/App.js
+++ f1-dashboard-frontend/src/App.js
@@ -1,32 +1,44 @@
 //https://www.youtube.com/watch?v=kTWJypbcSLI
-import React, {useEffect, useState, useRef, useMemo} from 'react';
-import logo from './logo.svg';
-import {Countdown} from './Countdown.js'
+import React, {useEffect, useState } from 'react';
+import Header from './header';
+import { Countdown } from './Countdown.js';
+import { ScheduleTable } from './ScheduleTable.js'
 import './App.css';
+
 
 function App() {
 		const [raceInfo, setRaceInfo] = useState([])
 		const [eventName, setEventName] = useState([])
 		const [eventDate, setEventDate] = useState([])
+		const [eventTimeInfo, setEventTimeInfo] = useState([])
 
 		useEffect(() => {
 				fetch('http://localhost:8000/upcoming')
 						.then(res => res.json())
 						.then(data => {
-								setRaceInfo(data.event)
+								setEventTimeInfo(data.events);
+								setRaceInfo(data.race);
+								setEventName(Object.keys(data.events.next)[0].toString());
 
-								const event_name = Object.keys(data.next_event)[0].split("_")[0].toUpperCase()
-								setEventName(event_name)
-
-								const eventDate = new Date(Object.values(data.next_event)[0]);
+								const eventDate = new Date(Object.values(data.events.next)[0]);
 								setEventDate(eventDate)
 						})
 		},[])
 
 		return <div>
-							 <h1>{raceInfo.year} {raceInfo.race} - Round {raceInfo.round}</h1>
-							 <Countdown date={eventDate} />
-							 <h2>{eventName}: {eventDate.toLocaleString()}</h2>
+							 <Header/>
+							 <div className="logo">
+							 </div>
+							 <div className="raceHeader">
+									 <h1>{raceInfo.year} {raceInfo.race} - {eventName}</h1>
+							 </div>
+
+							 <div className="countdown-timer">
+									 <Countdown date={eventDate} />
+							 </div>
+							 <div className="eventInfo">
+									 <h2>{eventDate.toLocaleString()}</h2>
+							 </div>
 					 </div>;
 }
 
