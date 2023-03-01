c1e1dc276774db75969a944b51351c0dee38da50
ColonelPan1k <kyle.goldbeck@gmail.com>
2023-03-01 15:57:39 -0500

    feat: reformat upcoming events with correct names

--- prototype/src/upcoming.py
+++ prototype/src/upcoming.py
@@ -10,18 +10,18 @@ def utc_to_local(utc_dt):
 
 
 def create_time_data(d: dict[str, any]):
-    keys = [
-        ["date", "time"],
-        ["fp1_date", "fp1_time"],
-        ["fp2_date", "fp2_time"],
-        ["fp3_date", "fp3_time"],
-        ["quali_date", "quali_time"],
-        ["sprint_date", "sprint_time"],
-    ]
+    keys = {
+        "Race": ["date", "time"],
+        "Free Practice 1": ["fp1_date", "fp1_time"],
+        "Free Practice 2": ["fp2_date", "fp2_time"],
+        "Free Practice 3": ["fp3_date", "fp3_time"],
+        "Qualifying": ["quali_date", "quali_time"],
+        "Sprint": ["sprint_date", "sprint_time"],
+    }
 
     data = {}
 
-    for key_group in keys:
+    for key, key_group in keys.items():
         date_times = [d.get(k) for k in key_group]
         if not date_times[0] and not date_times[1]:
             continue
@@ -30,13 +30,14 @@ def create_time_data(d: dict[str, any]):
             str(date_times[0]) + " " + str(date_times[1]), TIME_FORMAT
         )
 
-        data.update({key_group[0]: utc_to_local(full_date).strftime(TIME_FORMAT)})
+        data.update({key: utc_to_local(full_date).strftime(TIME_FORMAT)})
 
     return data
 
 
 def process_upcoming_event_data():
     event = get_upcoming_event()[0]
+
     data = {
         "raceId": event.get("raceId"),
         "race": event.get("name"),
@@ -50,15 +51,41 @@ def process_upcoming_event_data():
 
 def calculate_upcoming_sub_event():
     event_data = process_upcoming_event_data()
+    time_data = event_data.pop("time_data")
     now = datetime.datetime.now()
 
-    dates = {}
-    for event, date in event_data.get("time_data").items():
-        diff = datetime.datetime.strptime(date, TIME_FORMAT) - now
-        dates.update({event: diff})
+    dates = {"past": {}, "upcoming": {}, "current": {}}
 
-    return {
-        min(dates, key=dates.get): event_data["time_data"].get(
-            min(dates, key=dates.get)
-        )
-    }
+    for event, date in time_data.items():
+        diff = datetime.datetime.strptime(date, TIME_FORMAT) - now
+
+        # This is a mess
+        if diff <= datetime.timedelta(days=-1, seconds=82800):
+            dates.get("past").update({event: date})
+        elif diff >= datetime.timedelta(days=0):
+            dates["upcoming"].update({event: date})
+        else:
+            dates["current"].update({event: date})
+
+    if not dates:
+        return {}
+
+    key = min(dates["upcoming"], key=dates["upcoming"].get)
+    dates.update({"next": {key: dates["upcoming"].get(key)}})
+
+    return {"race": event_data, "events": dates}
+
+
+def get_upcoming_events_as_table():
+    race = get_upcoming_event()[0]
+    time_data = create_time_data(race)
+    formatted = []
+
+    print(time_data)
+
+    for key, value in time_data.items():
+        date, time = value.split(" ")[0], value.split(" ")[1]
+
+        formatted.append({"event_name": key, "date": date, "time": time})
+
+    return formatted
